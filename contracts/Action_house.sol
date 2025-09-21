e958a512bff482147e4905da56ede73d7b1f4400
Mohit8269 <dangimohit958@gmail.com>
2025-09-21 23:17:16 +0530

    Update Action_house.sol

--- contracts/Action_house.sol
+++ contracts/Action_house.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: mtl
+// SPDX-License-Identifier: 
 
 pragma solidity ^0.8.19;
 
@@ -1029,6 +1029,7 @@ contract Project {
         return (settledCount, totalFeesCollected);
     }
 }
+
 
 
 
